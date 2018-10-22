3aae053b25106b1238b0d78f0b1647c76ca83485
sandond77 <sandu@ucdavis.edu>
2018-10-22 15:11:28 -0700

    added themeProvider and styled-components to Page.js

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,35 +1,42 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
-import styled from 'styled-components';
 
-const MyButton = styled.button`
-	background: red;
-	font-size: ${props => (props.huge ? '100px' : '50px')};
+const StyledPage = styled.div`
+	background: white;
+	color: ${ props => props.theme.black};
+`
 
-	.poop{
-		font-size: 100px;
-	}
-`;
+const theme = { 
+	red: '#FF0000',
+	black: '#393939',
+	grey: '#3A3A3A',
+	lightgrey: '#E1E1E1',
+	offWhite: '#EDEDED',
+	maxWidth: '1000px',
+	bs: '0 12px 24px 0 rgba(0,0,0, 0.09)',
+
+};
+
+const Inner = styled.div`
+ max-width: ${ props => props.theme.maxWidth};
+ margin:0 auto;
+ padding: 2rem;
+`
 
 class Page extends Component {
 	render() {
 		return (
-			<div>
-				<Meta />
-				<Header />
-				{ this.props.children }
-				<MyButton huge='100'>
-					Click Me
-					<span className='poop'>poop</span>
-				</MyButton>
-				<MyButton>
-					Click Me
-					<span className='poop'>poop</span>
-				</MyButton>
-			</div>
+			<ThemeProvider theme={theme}>
+				<StyledPage>
+					<Meta />
+					<Header /> 
+					<Inner>{ this.props.children }</Inner>
+				</StyledPage>
+			</ThemeProvider>
 		);
 	}
 }
 
-export default Page;
+export default Page; 
