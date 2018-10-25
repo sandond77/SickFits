65efc777de3ef7a767e246ef254101add8f338e9
sandond77 <sandu@ucdavis.edu>
2018-10-24 21:45:41 -0700

    added initial code in backend/src/index.js for the initialization of the graphql yoga server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,18 @@
 // let's go!
+require('dontenv').config({ path: 'variables.env'});
+const createServer = require('createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// Use express middleware to handle cookies
+// Use express middleware to populate current user
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    }, deets => {
+        console.localStorage(`Server is now running on port http:/localhost:${deets.port}`);
+    }
+})
