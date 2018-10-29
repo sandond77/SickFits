48d5136c095e154f06a6c2f03f6164ed00c28170
sandond77 <sandu@ucdavis.edu>
2018-10-29 11:16:58 -0700

    turned items.js into react component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,9 @@
 import Link from 'next/link';
+import Items from '../components/items';
 
 const Home = props => (
 	<div>
-		<p>Hey!</p>
+		<Items />
 	</div>
 );
 
--- frontend/pages/items.js
+++ frontend/pages/items.js
@@ -1,3 +1,16 @@
-import Home from './index';
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
 
-export default Home;
+class items extends Component {
+	render() {
+		return (
+			<div>
+				<p>	
+					Items!
+				</p>
+			</div>
+		);
+	}
+}
+
+export default items;
