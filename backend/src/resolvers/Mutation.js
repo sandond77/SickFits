04d0d013a7d7fa3d51606c1f7d85f8f1c06f8caf
sandond77 <sandu@ucdavis.edu>
2018-10-28 18:25:54 -0700

    removed createdAt and updatedAt fields from prisma model and redeployed

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,7 +1,17 @@
 const Mutations = { 
-	createItem(parents, AbortSignal, ctx, info){
-		const item;
-	}
+	async createItem(parents, AbortSignal, ctx, info){
+
+		const item = await ctx.db.mutation.createItem ({
+			data: {
+				...args //using es6 to spread the arguments
+			}
+		}, info);
+
+		console.log(item);
+		
+		return item;
+	} 
+	
 };
 
 module.exports =  Mutations;
