579b8424fbc1311425c4704c52bcb8a88bfa9841
sandond77 <sandu@ucdavis.edu>
2018-10-28 17:48:34 -0700

    added and tested first query in Query.js

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,8 @@
-const Query = {};
+const Query = {
+	dogs(parent, args, ctx, info){ //parent schema, arguments, context, info
+		return [{ name: 'Snickers '}, { name: 'Sunny'}];
+	}
+};
 
 module.exports = Query;
+ 
