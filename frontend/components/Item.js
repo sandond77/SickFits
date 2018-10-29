f0220fed816a5c495ed5e2ed5f7b4951bb917fb0
sandond77 <sandu@ucdavis.edu>
2018-10-29 15:32:50 -0700

    added line for images in Item.js

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -13,6 +13,7 @@ class Item extends Component {
 
 		return (
 			<ItemStyles>
+				{item.image && <img src={item.image} alt={item.title} />}
 				<Title>
 					<Link href={{
 						pathname: '/item', 
